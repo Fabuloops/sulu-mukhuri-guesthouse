9cc4b8d1ec8534c0d23b6cd21781862ef5549e37
Fabuloops <fabrice.pantanella@gmail.com>
2018-02-19 15:55:43 +0100

    Footer + Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,7 @@ var gulp = require('gulp');
 gulp.task('default', function() {
 	gulp.src('app/Resources/public/**').pipe(gulp.dest('web/assets'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch('app/Resources/public/**', ['default']);
+});
